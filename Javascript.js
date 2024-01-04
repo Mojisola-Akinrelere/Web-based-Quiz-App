5fa85b6df17a0908490d1f12f4f6297931d3f98d
Mojisola-Akinrelere <mojisolaoakinrelere@gmail.com>
2024-01-04 16:09:47 +0100

    Added Quiz Questions with the answers

---
+++ Javascript.js
@@ -0,0 +1,110 @@
+const questions = [
+{
+    questions: "In the UK, the abbreviation NHS stands for National what Service?"
+    answers: [
+        { text: "Humanity", correct: false},
+        { text: "Honour", correct: false},
+        { text: "Health", correct: true},
+        { text: "Household", correct: false},
+    ]
+},
+{
+    questions: "What does A.I. stand for?"
+    answers: [
+        { text: "Artificial information", correct: false},
+        { text: "Artificial intelligence", correct: true},
+        { text: "Anti intelligence", correct: false},
+        { text: "Anti information", correct: false},
+    ]  
+},
+{
+    questions: "Obstetrics is a branch of medicine particularly concerned with what?"
+    answers: [
+        { text: "Childbirth", correct: true},
+        { text: "Broken bones", correct: false},
+        { text: "Heart conditions", correct: false},
+        { text: "Old Age", correct: false},
+    ]
+},
+{
+    questions: "Which Disney character famously leaves a glass slipper behind at a royal ball?"
+    answers: [
+        { text: "Pocahontas", correct: false},
+        { text: "Cinderella", correct: true},
+        { text: "Sleeping Beauty", correct: false},
+        { text: "Elsa", correct: false},
+    ]  
+},
+{
+    questions: "What does the word loquacious mean?"
+    answers: [
+        { text: "Angry", correct: false},
+        { text: "Shy", correct: false},
+        { text: "Beautiful", correct: false},
+        { text: "Chatty", correct: true},
+    ]  
+},
+{
+    questions: "World-wide, what language is used the most on the internet?"
+    answers: [
+        { text: "German", correct: false},
+        { text: "English", correct: true},
+        { text: "Spanish", correct: false},
+        { text: "Chinese", correct: false},
+    ]  
+},
+{
+    questions: "Which of these is not a social media platform?"
+    answers: [
+        { text: "Instagram", correct: false},
+        { text: "Twitter", correct: false},
+        { text: "VLC", correct: true},
+        { text: "Youtube", correct: false},
+    ]  
+},
+{
+    questions: "Which of these is not a social media platform?"
+    answers: [
+        { text: "Instagram", correct: false},
+        { text: "Twitter", correct: false},
+        { text: "VLC", correct: true},
+        { text: "Youtube", correct: false},
+    ] 
+},
+{
+    questions: "Which of these is not a social media platform?"
+    answers: [
+        { text: "Instagram", correct: false},
+        { text: "Twitter", correct: false},
+        { text: "VLC", correct: true},
+        { text: "Youtube", correct: false},
+    ] 
+},
+{
+    questions: "Which of these is not a social media platform?"
+    answers: [
+        { text: "Instagram", correct: false},
+        { text: "Twitter", correct: false},
+        { text: "VLC", correct: true},
+        { text: "Youtube", correct: false},
+    ] 
+},
+{
+    questions: "Which of these is not a social media platform?"
+    answers: [
+        { text: "Instagram", correct: false},
+        { text: "Twitter", correct: false},
+        { text: "VLC", correct: true},
+        { text: "Youtube", correct: false},
+    ] 
+},
+{
+    questions: "Which of these is not a social media platform?"
+    answers: [
+        { text: "Instagram", correct: false},
+        { text: "Twitter", correct: false},
+        { text: "VLC", correct: true},
+        { text: "Youtube", correct: false},
+    ] 
+}   
+]
